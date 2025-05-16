1058de934c1aedeb00187b11cb335302c4fa3a7b
JC <jcdotnet@hotmail.com>
2025-05-16 13:08:43 +0200

    feat :: added favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('favicon.png') }}">
     <title>José Carlos Román Rubio || Software Developer</title>
     @vite('resources/css/app.css')
 </head>
