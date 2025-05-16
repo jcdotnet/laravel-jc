defc723c6c09f06f6fc40d7cbd6405dda407794f
JC <jcdotnet@hotmail.com>
2025-05-16 13:26:12 +0200

    fix (home page) :: icons size on mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,17 +6,17 @@
             <ul>
                 <li class="inline-block pr-1">
                     <a href="https://www.facebook.com/jc11235" title="Follow on Facebook" target="_blank">
-                        <i class="fa-brands fa-square-facebook text-xl md:text-3xl text-(--color-facebook)"></i>
+                        <i class="fa-brands fa-square-facebook text-2xl sm:text-3xl text-(--color-facebook)"></i>
                     </a>
                 </li>
                 <li class="inline-block pr-1">
                     <a href="https://x.com/jcdev_" title="Follow on X" target="_blank">
-                        <i class="fa-brands fa-square-x-twitter text-xl md:text-3xl text-black"></i>
+                        <i class="fa-brands fa-square-x-twitter text-2xl sm:text-3xl text-black"></i>
                     </a>
                 </li>
                 <li class="inline-block">
                     <a href="https://www.linkedin.com/in/romanrubio/" title="Follow on LinkedIn" target="_blank">
-                        <i class="fa-brands fa-linkedin text-xl md:text-3xl text-(--color-linkedin)"></i>
+                        <i class="fa-brands fa-linkedin text-2xl sm:text-3xl text-(--color-linkedin)"></i>
                     </a>
                 </li>
             </ul>
