8a1529681130cb91fd5a4599c06bda85ed3c62f0
JC <jcdotnet@hotmail.com>
2025-05-21 14:00:01 +0200

    feat (DB Migration) :: created pivot table

--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\ExperienceType;
+use App\Models\Skill;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\ExperienceType;
 
 class Experience extends Model
 {
@@ -12,6 +13,10 @@ class Experience extends Model
     use HasFactory;
 
     public function experience_type() {
-        return $this->belongsTo(ExperienceType::class);
+        return $this->hasOne(ExperienceType::class);
+    }
+    
+    public function skills() {
+        return $this->belongsToMany(Skill::class);
     }
 }
--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Experience;
 use Illuminate\Database\Eloquent\Model;
 
 class Skill extends Model
 {
-    //
+    public function experiences() {
+        return $this->belongsToMany(Experience::class);
+    }
 }
