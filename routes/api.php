b9edafd2a3881f48171f2a0da9756320612327b0
JC <jcdotnet@hotmail.com>
2025-08-19 14:28:34 +0200

    change(API) :: updated endpoints

--- routes/api.php
+++ routes/api.php
@@ -7,62 +7,51 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/categories', function ( Request $request){
-    $categories = App\Models\Category::all(); 
+/**
+ * TO-DO, refactor: to move functions to SkillRestController to clean the routes code
+ */
+Route::get('/skills',function() {
+    $skills = App\Models\Skill::all();
+    return response()->json($skills); //, 200);  
+});
+
+Route::get('/skills/{id}',function($id) {
+    $skill= App\Models\Skill::find($id);
+    return response()->json($skill); //, 200); 
+});
+
+Route::get('/skills/{id}/experiences',function($id) {
+    $skill = App\Models\Skill::where('id', '=', $id)->with('experiences')->get();
+    return response()->json($skill); 
+});
+
+Route::get('/categories',function() {
+    $categories = App\Models\Category::all();
+    return response()->json($categories); 
+});
+
+Route::get('/categories/skills',function() {
+    $categories = App\Models\Category::where('slug', '!=', 'uncategorized')->with('skills')->get();
     return response()->json($categories);
 });
 
-/**
- * TO-DO, refactor: to move functions to SkillController to clean the routes code
- */
-function get_skills( $category_id ) {
-    $skills = App\Models\Skill::where('category_id', $category_id)->get();
-    if ( !$skills ) {
-        return response()->json(['error' => 'Skills Not Found'], 404);
-    }
-    return $skills;
-}
-
-Route::get('/skills',function() {
-    $skills = App\Models\Skill::all();
-    return response()->json($skills);
+Route::get('/categories/{id}',function($id) {
+    $category = App\Models\Category::find($id);
+    return response()->json($category);  
 });
 
-// Route::get('/skills/{id}',function($id) {
-// // TO-DO
-// });
-
-// Route::get('/skills/{id}/experiences',function($id) {
-// // TO-DO
-// });
-
-Route::get('/skills/categories',function() {
-    $categories = App\Models\Category::where('slug', '!=', 'uncategorized')->get()->toArray();
-    $output = [];
-    foreach ($categories as $category) {
-        $output []= [
-            'title'     => $category['name'],
-            'skills'    => get_skills( $category['id'] )
-        ];
-    }
-    return response()->json($output);
-});
-
-Route::get('/skills/category/{categoryId}',function($categoryId) { // maybe Route::get('/categories/{id}/skills',function($id)
+Route::get('/categories/{id}/skills',function($id) {
     
-    $skills = App\Models\Skill::where('category_id', $categoryId)->get();
-    if ( !$skills ) {
-        return response()->json(['error' => 'Skills Not Found'], 404);
-    }
-    return response()->json($skills);
+    $category = App\Models\Category::where('id', '=', $id)->with('skills')->get();
+    return response()->json($category);
 });
 
-Route::get('/skills/slug/{slug}',function($slug) { // maybe Route::get('/categories/slug/{slug}/skills',function($slug)
+Route::get('/categories/slug/{slug}',function($slug) {
     $category = App\Models\Category::where('slug', $slug)->get();
-    if ( $category->isEmpty()) {
-        return response()->json(['error' => 'Category Not Found'], 404);
-    }
-    $skills = get_skills( $category->first()->id );
+    return response()->json($category);
+});
 
-    return response()->json($skills);
+Route::get('/categories/slug/{slug}/skills',function($slug) {
+    $category = App\Models\Category::where('slug', $slug)->with('skills')->get();
+    return response()->json($category);
 });
